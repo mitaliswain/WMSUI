f35917a0d922c810c811076101204ba5ab36d2e9
Mitali Swain <mitali.swain@gmail.com>
2014-08-23 08:33:57 -0700

    fixing accoridion and modal

--- app/assets/javascripts/controller/shipment_list_angular.js
+++ app/assets/javascripts/controller/shipment_list_angular.js
@@ -4,6 +4,7 @@ function ShipmentListCtrl($scope, $http,$location) {
   var warehouse = $location.search().warehouse;
   var shipment  =  $location.search().shipment;
   var client    = $location.search().client; 
+    $scope.shipment = 'new shipment';
   var url = '/shipmentdetails.json?client=' + client + '&warehouse='+ warehouse;		
   $http.get(url).success(function(data) {
     $scope.shipment_headers = data;
