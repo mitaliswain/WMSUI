b5cfa2e793d4fc658e0f0beddf6aec641372b0a8
Mitali Swain <mitali.swain@gmail.com>
2014-09-05 11:22:54 -0700

    09/05/2014

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+
 //= require_tree ./bootstrap
 //= require_tree ./angular
+//= require_tree ./ngTable
 //= require_tree ./xeditable
 
 //= require ./controller/app.js.erb
