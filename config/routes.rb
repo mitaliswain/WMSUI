33d4a4cb43569b905eb0a66601de53eb7c3bacaa
babu <babuvasa@gmail.com>
2015-10-11 03:41:09 -0700

    commented redirection

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Rails.application.routes.draw do
     post '', action: 'create'
   end
 
-  namespace :login do
+  namespace :logins do
     get '', action: 'index'
     get  ':id', action: 'show'
     post ':id', action: 'update'
